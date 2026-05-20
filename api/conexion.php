63df8202f0edb87acca9c3c4b1f02d24cc743102
Ubuntu <ubuntu@ip-172-31-37-170.us-east-2.compute.internal>
2026-05-20 04:25:52 +0000

    Fix: conexion.php usar XE en lugar de XEPDB1

--- api/conexion.php
+++ api/conexion.php
@@ -3,7 +3,7 @@
 
 $username = 'usuario_finanzas'; 
 $password = 'admin1234';
-$database = '//oracle_db:1521/XEPDB1'; 
+$database = '//oracle_db:1521/XE'; 
 
 // Intentamos abrir la conexión con Oracle
 $conexion = @oci_pconnect($username, $password, $database, 'AL32UTF8');
